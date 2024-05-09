8825d6934e9072241b2ccb359420addc498928b9
Johannes Merl <johannes.merl@mischok.de>
2024-05-09 09:31:56 +0200

    weights #3

--- lambda/src/LambdaDatasets/IrisDataset.hs
+++ lambda/src/LambdaDatasets/IrisDataset.hs
@@ -56,10 +56,10 @@ lE =
       maxDepth = 9,
       weights =
         ExpressionWeights
-          { lambdaSpucker = 1,
-            lambdaSchlucker = 2,
-            symbol = 30,
-            variable = 10,
+          { lambdaSpucker = 0,
+            lambdaSchlucker = 10,
+            symbol = 100,
+            variable = 5,
             constant = 5
           }
     }
