8a04da73f3efeefb50c9f598eb6657c2c3772409
Johannes Merl <johannes.merl@mischok.de>
2024-05-09 09:24:21 +0200

    variation 4

--- lambda/src/LambdaDatasets/IrisDataset.hs
+++ lambda/src/LambdaDatasets/IrisDataset.hs
@@ -56,10 +56,10 @@ lE =
       maxDepth = 8,
       weights =
         ExpressionWeights
-          { lambdaSpucker = 1,
-            lambdaSchlucker = 2,
-            symbol = 30,
-            variable = 10,
+          { lambdaSpucker = 10,
+            lambdaSchlucker = 1,
+            symbol = 20,
+            variable = 100,
             constant = 5
           }
     }
